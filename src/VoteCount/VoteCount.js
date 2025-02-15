8c467dfd56c80b3886416244efb0bc3eef63825a
Montana Pfeifer <montanapfeifer@hotmail.com>
2025-02-15 10:36:58 -0700

    feat: add: thumbs up and thumbs down to vote count container

--- src/VoteCount/VoteCount.js
+++ src/VoteCount/VoteCount.js
@@ -1,7 +1,12 @@
+import React from 'react';
+import './VoteCount.css';
+
 const VoteCount = ({ votes }) => {
   return (
-    <div className="vote-count">
-      {votes}
+    <div className="vote-container">
+      <button className="vote-btn">👎</button>
+      <span className="vote-count">{votes}</span>
+      <button className="vote-btn">👍</button>
     </div>
   );
 };
