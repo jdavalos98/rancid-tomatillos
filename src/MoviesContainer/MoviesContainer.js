d1a61e80d79d040529aa6dfe5a446de9dfb1fc72
Joel Davalos <jsdavalos@gmail.com>
2025-02-18 12:18:53 -0700

    Feat: Added upvote and downvote functionality into movies container

--- src/MoviesContainer/MoviesContainer.js
+++ src/MoviesContainer/MoviesContainer.js
@@ -3,14 +3,17 @@ import React from 'react'
 import MoviePoster from "../MoviePoster/MoviePoster";
 import VoteCount from "../VoteCount/VoteCount"
 
-function Movies({movies}) {
+function Movies({movies, upvoteMovie, downvoteMovie}) {
   return (
       <section className='MoviesContainer'>
         <div className='movies-grid'>
           {movies.map((movie) => (
              <div key={movie.id} className="movie-card">
              <MoviePoster movie={movie} />
-             <VoteCount votes={movie.vote_count} />
+             <VoteCount 
+                votes={movie.vote_count}
+                upvote={() => upvoteMovie(movie.id)}
+                downvote={() => downvoteMovie(movie.id)} />
            </div>
           ))}
           </div>
