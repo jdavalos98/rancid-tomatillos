0c524907974f98af12611d4697d5f2cb3ca8438e
Joel Davalos <jsdavalos@gmail.com>
2025-02-18 16:49:02 -0700

    Added funcitnality to save updated vote count so it persists on page load

--- src/App/App.js
+++ src/App/App.js
@@ -8,7 +8,15 @@ import movieDetails from '../data/movie_details';
 import Movies from '../MoviesContainer/MoviesContainer';
 
 function App() {
-  const [movies, setMovies] = useState(moviePosters);
+  const [movies, setMovies] = useState(() => {
+    
+    const savedMovies = localStorage.getItem('movies');
+    return savedMovies ? JSON.parse(savedMovies) : moviePosters;
+  });
+
+  useEffect(() => {
+    localStorage.setItem('movies', JSON.stringify(movies));
+  }, [movies]);
 
   const upvoteMovie = (id) => {
     setMovies(movies.map(movie => 
