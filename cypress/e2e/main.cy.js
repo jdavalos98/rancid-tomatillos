9ede417c576edfd648ce291a2465a251655afbde
Joel Davalos <jsdavalos@gmail.com>
2025-02-20 20:25:39 -0700

    Added before each statment tointercept api call

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,12 +1,14 @@
-// Mock data to use for testing:
-// import posters from '../fixtures/movie_posters.json' (we've added mock data to this file for you!)
+import posters from '../fixtures/movie_posters.json'
 // import details from '../fixtures/movie_details.json' (you will need to add your own mock data to this file!)
 
 describe('Main Page', () => {
-  it('displays title on page load', () => {
-    // hint: you'll want to add an intercept here if you are making a network request on page load!
-    cy.visit('http://localhost:3000/')
-    .get('h1')
-    .contains('rancid tomatillos')
-  })
+  beforeEach(() => {
+    cy.intercept('GET', 'https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies', {
+      statusCode: 200,
+      body: posters
+    }).as('getMovies');
+
+    cy.visit('http://localhost:3000/');
+  });
+
 })
